32ba3b24182f063f1357b05ee97fb1be3cb67862
Crowdbotics <engineering@crowdbotics.com>
2023-04-04 00:20:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/odd_star_dev_74040/MainActivity.java
@@ -0,0 +1,15 @@
+package com.odd_star_dev_74040;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "odd_star_dev_74040";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-star-dev-74040",
+  url: "https://odd-star-dev-74040.botics.co",
+  api: "https://odd-star-dev-74040.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
